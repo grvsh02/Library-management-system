f864699ac58aae754fd68227cb28f666b9561258
Gaurav Sharma <gaurav021201@gmail.com>
2022-11-05 14:36:30 +0530

    added display function

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ class book:
         self.code = code
         self.price = price
 
+    def display(self):
+        print(f"Name: {self.name} \nAuthor: {self.author} \nCode: {self.code} \nPrice: {self.price}")
+
 
 class student:
     def __init__(self, name, rollno, branch):
@@ -15,10 +18,20 @@ class student:
         self.rollno = rollno
         self.branch = branch
 
+    def display(self):
+        print(f"Name: {self.name} \nRoll No: {self.rollno} \nBranch: {self.branch}")
+
 
 class library(book, student):
+
     def __init__(self, datein, dateout, name, author, code, price, roll, branch):
         book.__init__(self, name, author, code, price)
         student.__init__(self, name, roll, branch)
         self.datein = datein
         self.dateout = dateout
+
+    def display(self):
+        book.display(self)
+        student.display(self)
+        print(f"Date In: {self.datein} \nDate Out: {self.dateout}")
+
