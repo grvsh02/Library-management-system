3b909181183db29df8c8cdc543efd93bd08cfbb2
Prahsant Gehlot <gehlotprashant968@gmail.com>
2022-11-18 13:31:30 +0530

    Added two models to book app

--- shivaya/book/models.py
+++ shivaya/book/models.py
@@ -1,11 +1,22 @@
 from django.db import models
+from datetime import datetime, timedelta
+import uuid
 
 # Create your models here.
 
-class Destination(models.Model):
-    name = models.CharField(max_length=100)
+class Books(models.Model):
+    book_name = models.CharField(max_length=100)
     book_id = models.IntegerField()
     author = models.CharField(max_length=100)
-
+    is_available = models.BooleanField(default=True)
+    summary = models.TextField(max_length=1000, help_text='Summary of the book', null=True, blank=True)
     def __str__(self):
-        return self.name
+        return self.book_name
+
+class BookInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID')
+    book = models.ForeignKey('Books', on_delete=models.CASCADE, null=True)
+    book_number = models.PositiveIntegerField(null=True)
+    is_borrowed = models.BooleanField(default=False)
+    def __str__(self):
+        return f"{self.id} ({self.book})"
