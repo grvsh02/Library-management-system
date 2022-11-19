4c506973630e20c2a49e9ec49b937e47a8abd9b3
yakatyansh <ykpetrolhead08@outlook.com>
2022-11-19 14:34:17 +0530

    added components

---
+++ shakti/src/components/button.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import styled from "@emotion/styled";
+
+
+const ButtonType = styled('div')`
+    
+    
+    
+    
+`;
+
+
+
+
+const Button = () => {
+
+}
+
+export default Button
---
+++ shakti/src/home.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Home() {
+
+    return (
+        <div className="bg-gray-700">
+            <h1 className="bg-green-700 text-4xl ">Manage Subscription</h1>
+            <div className="bg-amber-600 text-6xl w-1/5 min-h-screen">
+                <ul>
+                    <li>Books</li>
+                    <li>Authors</li>
+                    <li>Genres</li>
+                </ul>
+            </div>
+            body
+        </div>
+    );
+}
