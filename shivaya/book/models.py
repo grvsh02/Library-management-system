00d8366f8f24d2b35aa3d3167d9d5a271ec4f092
Prahsant Gehlot <gehlotprashant968@gmail.com>
2022-11-19 16:38:04 +0530

    Changed book models

--- shivaya/book/models.py
+++ shivaya/book/models.py
@@ -1,22 +1,29 @@
 from django.db import models
 from datetime import datetime, timedelta
-import uuid
-
-# Create your models here.
+from ..user.models import Student
 
 class Books(models.Model):
+    id = models.AutoField(primary_key=True, help_text='Unique ID')
     book_name = models.CharField(max_length=100)
     book_id = models.IntegerField()
     author = models.CharField(max_length=100)
-    is_available = models.BooleanField(default=True)
+    is_borrowed = models.BooleanField(default=False)
     summary = models.TextField(max_length=1000, help_text='Summary of the book', null=True, blank=True)
+    cost = models.IntegerField()
     def __str__(self):
         return self.book_name
 
-class BookInstance(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID')
-    book = models.ForeignKey('Books', on_delete=models.CASCADE, null=True)
-    book_number = models.PositiveIntegerField(null=True)
-    is_borrowed = models.BooleanField(default=False)
+def get_returndate():
+    return datetime.today() + timedelta(days=6)
+
+class Book_Issue(models.Model):
+    student = models.ForeignKey(Student.fullname, on_delete=models.CASCADE)
+    book = models.ForeignKey(Books.book_name, on_delete=models.CASCADE)
+    issue_date = models.DateTimeField(auto_now=True, help_text="Date the book is issued")
+    due_date = models.DateTimeField(default=get_returndate(),help_text="Date the book is due to")
+    date_returned = models.DateField(null=True, blank=True, help_text="Date the book is returned")
+    remarks_on_issue = models.CharField(max_length=100, default="Book in good condition", help_text="Book remarks/condition during issue")
+    remarks_on_return = models.CharField(max_length=100, default="Book in good condition", help_text="Book remarks/condition during return")
+
     def __str__(self):
-        return f"{self.id} ({self.book})"
+        return str(self.student) + " borrowed " + str(self.book)
--- shivaya/user/models.py
+++ shivaya/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class Student(models.Model):
+    fullname = models.CharField(max_length=100)
+    rollno = models.IntegerField()
+    email = models.EmailField(unique=True, verbose_name='Email Address')
+    password = models.CharField(max_length=100)
+    mobileno = PhoneNumberField(null=True, blank=True)
+    def __str__(self):
+        return self.fullname
