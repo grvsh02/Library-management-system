026f5cbf5ce3aa4782a78a5d0f278a6c1717817f
Prahsant Gehlot <gehlotprashant968@gmail.com>
2022-11-20 09:26:17 +0530

    fixed one certain issue in book/models.py

--- shivaya/book/models.py
+++ shivaya/book/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from datetime import datetime, timedelta
-from ..user.models import Student
+from user.models import Student
 
 class Books(models.Model):
     id = models.AutoField(primary_key=True, help_text='Unique ID')
@@ -17,8 +17,8 @@ def get_returndate():
     return datetime.today() + timedelta(days=6)
 
 class Book_Issue(models.Model):
-    student = models.ForeignKey(Student.fullname, on_delete=models.CASCADE)
-    book = models.ForeignKey(Books.book_name, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    book = models.ForeignKey(Books, on_delete=models.CASCADE)
     issue_date = models.DateTimeField(auto_now=True, help_text="Date the book is issued")
     due_date = models.DateTimeField(default=get_returndate(),help_text="Date the book is due to")
     date_returned = models.DateField(null=True, blank=True, help_text="Date the book is returned")
